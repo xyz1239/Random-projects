4ed33d038094a342a3cb1e01177d572c1971afec
xyz1239 <maxyoyo5@gmail.com>
2025-04-02 12:11:08 +1100

    Updated resteruant selector code complete need to add docuemntation

--- Restaurant_selector.py
+++ Restaurant_selector.py
@@ -1,4 +1,3 @@
-from typing import List, Tuple
 from dataclasses import dataclass
 
 @dataclass
@@ -7,8 +6,8 @@ class DietRestrictions:
     vegan: bool
     glutenfree: bool
 
-    def match(self, required: DietRestrictions) -> bool:
-        if required.veg and not self.vegetarian:
+    def match(self, required: 'DietRestrictions') -> bool:
+        if required.vegetarian and not self.vegetarian:
             return False
         if required.vegan and not self.vegan:
             return False
@@ -21,19 +20,11 @@ class Restaurant:
     name: str
     diet: DietRestrictions
 
-RR = [Restaurant("joe's pizza", DietRestrictions(False, False, False))]
-[Restaurant("Main Street Pizza Company", DietRestrictions(True, False, True))]
-[Restaurant("Corner Café", DietRestrictions(True, True, True))]
-[Restaurant("Mama’s Fine Italian", DietRestrictions(True, False, False))]
-[Restaurant("The Chef’s Kitchen", DietRestrictions(True, True, True))]
-
-#RESTAURANTS: List[Tuple[str, bool,bool,bool]]= [
-("joe's pizza", False, False, False)
-("Main Street Pizza Company", True, False, True)
-("Corner Café", True, True, True)
-("Mama’s Fine Italian", True, False, False)
-("The Chef’s Kitchen", True, True, True)
-#]
+RR = [Restaurant("joe's pizza", DietRestrictions(False, False, False)),
+Restaurant("Main Street Pizza Company", DietRestrictions(True, False, True)),
+Restaurant("Corner Café", DietRestrictions(True, True, True)),
+Restaurant("Mama’s Fine Italian", DietRestrictions(True, False, False)),
+Restaurant("The Chef’s Kitchen", DietRestrictions(True, True, True))]
 
 
 def ask_yn(prompt:str) -> bool:
@@ -45,24 +36,19 @@ def ask_yn(prompt:str) -> bool:
             return False
         print("not a yes or no, please try again")
 
-#def can_eat_at(r: Tuple[str, bool,bool,bool], veg, vegan, glu: bool)-> bool:
-    if not veg or veg != r[1]:
-        return False
-    return True
+
 
 def diet_inputs():
     vegetarian: bool = ask_yn("Is anyone in you party vegetarian?: ")
-    vegan=bool(input("Is anyone in you party vegan?: "))
-    glutenfree=bool(input("Is anyone in you party glutenfree?: "))
+    vegan: bool = ask_yn("Is anyone in you party vegan?: ")
+    glutenfree: bool = ask_yn("Is anyone in you party glutenfree?: ")
 
     required = DietRestrictions(vegetarian, vegan, glutenfree)
     for r in RR:
         if r.diet.match(required):
             print(r.name)
 
-    #for r in RESTAURANTS:
-        #if can_eat_at(r,vegetarian, vegan, glutenfree):
-            print(r[0])
+
 
 
 
