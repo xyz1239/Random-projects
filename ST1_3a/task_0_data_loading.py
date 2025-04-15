c446ee4da5f6375bc76ccd43925b7ee2a36c2a35
Staplemoon38396 <alexhparnell@gmail.com>
2025-04-15 18:31:19 +1000

    this weird

--- ST1_3a/task_0_data_loading.py
+++ ST1_3a/task_0_data_loading.py
@@ -4,9 +4,11 @@ with open("students.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
+    
     delimiter = (",",)
     quotechar = ('"',)
 
     # Skip the header row
     next(reader)
     # Read the rest of the rows
+
