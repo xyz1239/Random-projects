ffb5481caeb0477595010865852e04eb33f0aaa3
Staplemoon38396 <alexhparnell@gmail.com>
2025-04-15 18:22:51 +1000

    added some new code ;)

--- ST1_3a/task_0_data_loading.py
+++ ST1_3a/task_0_data_loading.py
@@ -3,3 +3,14 @@ import csv
 open(
     "students.csv",
 )
+
+with open("students.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+    delimiter=",",
+    quotechar='"',
+
+    # Skip the header row
+    next(reader)
+    # Read the rest of the rows
