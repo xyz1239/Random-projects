0efeb1cdcf0d90dfb1f974b8228708a22c20d87a
Staplemoon38396 <alexhparnell@gmail.com>
2025-04-23 17:36:28 +1000

    removed waste

--- ST1_3a/task_0_data_loading.py
+++ ST1_3a/task_0_data_loading.py
@@ -4,12 +4,7 @@ with open("students.csv", "r") as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
-    
-    delimiter = (",",)
-    quotechar = ('"',)
 
     # Skip the header row
     next(reader)
     # Read the rest of the rows
-
-hello max
