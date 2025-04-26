a1ab86bdbc64e2c893a9a8a36c92a6da98c18b30
Staplemoon38396 <alexhparnell@gmail.com>
2025-04-26 12:39:53 +1000

    Student class created for project

--- ST1_3a/task_0_data_loading.py
+++ ST1_3a/task_0_data_loading.py
@@ -19,6 +19,11 @@ with open("courses.csv", newline="", encoding="utf-8") as file:
     print(tb.tabulate(data, headers=header, tablefmt="grid"))
 
 
-class students:
+class students: 
+    def __init__(self, student_id,name,student_type):
+        self.name = name
+        self.student_id = student_id
+        self.student_type = student_type
 
-    
+    def __str__(self):
+        return f"{self.name} {self.student_id} {self.student_type}"
