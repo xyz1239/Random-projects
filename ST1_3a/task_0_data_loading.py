624c71fd862a38ead72867bbdc46408fd3a9e9e5
Staplemoon38396 <alexhparnell@gmail.com>
2025-04-26 12:34:07 +1000

    Both now will print once we fix file location

--- ST1_3a/task_0_data_loading.py
+++ ST1_3a/task_0_data_loading.py
@@ -1,16 +1,24 @@
 import csv
 import tabulate as tb
 
+"cd . \ST1_3a\ "
 
 with open("students.csv", newline="", encoding="utf-8") as file:
     reader = csv.reader(file)
     header = next(reader)
     data = [row for row in reader]
 
-
 print(tb.tabulate(data, headers=header, tablefmt="grid"))
 
 
-    
+with open("courses.csv", newline="", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    data = [row for row in reader]
+
+    print(tb.tabulate(data, headers=header, tablefmt="grid"))
+
+
+class students:
 
     
